0461db6de6833c91a5c7ee551d60e68a677b517c
Mehran Dhn <FloydianMehran@live.com>
2020-07-23 13:37:59 +0430

    Initial Project Structure with awaitable inside loop

--- Program.cs
+++ Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace AsyncEnumerable
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             Console.WriteLine("Discovering AsyncEnumerable in C# 8.0");
+            Console.WriteLine("Step 1: Awaitable inside loop");
+            foreach (var dataPoint in await FetchIOTData())
+            {
+                Console.WriteLine(dataPoint);
+            }
+            Console.ReadLine();
+        }
+
+
+        static async Task<IEnumerable<int>> FetchIOTData()
+        {
+            List<int> dataPoints = new List<int>();
+            for (int i = 1; i <= 10; i++)
+            {
+                await Task.Delay(1000); 
+                dataPoints.Add(i);
+            }
+
+            return dataPoints;
         }
     }
 }
