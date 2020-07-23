9f8c7f0391fbdb1b2b81dfe642553b6b9f53338b
Mehran Dhn <FloydianMehran@live.com>
2020-07-23 13:30:42 +0430

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AsyncEnumerable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Discovering AsyncEnumerable in C# 8.0");
+        }
+    }
+}
