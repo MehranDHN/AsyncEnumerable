78516f7a0088f86eef2e27b866012aea0f1af417
Mehran Dhn <FloydianMehran@live.com>
2020-07-23 13:45:57 +0430

    Using IAsyncEnumerable

--- Program.cs
+++ Program.cs
@@ -10,15 +10,24 @@ namespace AsyncEnumerable
         {
             Console.WriteLine("Discovering AsyncEnumerable in C# 8.0");
             Console.WriteLine("Step 1: Awaitable inside loop");
-            foreach (var dataPoint in await FetchIOTData())
+            foreach (var dataPoint in await FetchIOTDataV1())
             {
                 Console.WriteLine(dataPoint);
             }
+            Console.WriteLine("Please press Enter to re-run the example using IAsyncEnumerable.");
             Console.ReadLine();
+            Console.WriteLine("Step 2: IAsyncEnumerable");
+            await foreach (var dataPoint in FetchIOTDataV2())
+            {
+                Console.WriteLine(dataPoint);
+            }
+
+            Console.ReadLine();
+
         }
 
 
-        static async Task<IEnumerable<int>> FetchIOTData()
+        static async Task<IEnumerable<int>> FetchIOTDataV1()
         {
             List<int> dataPoints = new List<int>();
             for (int i = 1; i <= 10; i++)
@@ -28,6 +37,15 @@ namespace AsyncEnumerable
             }
 
             return dataPoints;
+        }
+
+        static async IAsyncEnumerable<int> FetchIOTDataV2()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                await Task.Delay(1000);
+                yield return i;
+            }
         }
     }
 }
